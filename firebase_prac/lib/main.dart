cbbadc88321b4d5bb4161248d68b327e79be56e2
Muhammad Ibrar <muhammad49ibrar@gmail.com>
2023-01-01 20:06:57 +0500

    firebase_prac is modified

--- firebase_prac/lib/main.dart
+++ firebase_prac/lib/main.dart
@@ -3,12 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
-
-  await WidgetsFlutterBinding.ensureInitialized();
-
-    Firebase.initializeApp();
-
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
